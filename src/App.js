53a8cbd5b86a7483927913b64fa14f91327940db
Brandon Benoit <b.nutty@gmail.com>
2021-09-04 20:37:52 -0700

    added home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faGamepad, faSearch } from '@fortawesome/free-solid-svg-icons';
 import Navbar from './js/components/Navbar';
+import Home from './js/pages/Home';
+
+library.add(fab, faGamepad, faSearch);
 
 const App = () => {
-  return (
-    <div>
-      <Navbar/>
-    </div>
-  );
+	return (
+		<div>
+			<Navbar />
+			<Home />
+		</div>
+	);
 };
 
 export default App;
--- src/js/components/Navbar.jsx
+++ src/js/components/Navbar.jsx
@@ -1,35 +1,40 @@
 import React from 'react';
-import {
-    AppBar,
-    Toolbar,
-    Typography,
-    Button,
-    IconButton
-} from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu'
+import { AppBar, Toolbar, IconButton } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Navbar = () => {
-    return (
-        <AppBar position="sticky">
-            <Toolbar className="nav">
-                <div className="nav-left">
-
-                </div>
-                <div className="nav-center">
-                    <ul className="nav-list">
-                        <li className="nav-list-item">Home</li>
-                        <li className="nav-list-item">About</li>
-                        <li className="nav-list-item">Contact</li>
-                        <li className="nav-list-item">Write</li>
-                        <li className="nav-list-item">Logout</li>
-                    </ul>
-                </div>
-                <div className="nav-right">
-
-                </div>
-            </Toolbar>
-        </AppBar>
-    );
+	return (
+		<AppBar position='sticky'>
+			<Toolbar className='nav'>
+				<div className='nav-left'>
+					<IconButton>
+						<FontAwesomeIcon icon={['fab', 'xbox']} />
+					</IconButton>
+					<IconButton>
+						<FontAwesomeIcon icon={['fab', 'playstation']} />
+					</IconButton>
+					<IconButton>
+						<FontAwesomeIcon icon='gamepad' />
+					</IconButton>
+				</div>
+				<div className='nav-center'>
+					<ul className='nav-list'>
+						<li className='nav-list-item'>Home</li>
+						<li className='nav-list-item'>About</li>
+						<li className='nav-list-item'>Contact</li>
+						<li className='nav-list-item'>Write</li>
+						<li className='nav-list-item'>Logout</li>
+					</ul>
+				</div>
+				<div className='nav-right'>
+					<IconButton>
+						<FontAwesomeIcon icon='search' />
+					</IconButton>
+				</div>
+			</Toolbar>
+		</AppBar>
+	);
 };
 
 export default Navbar;
